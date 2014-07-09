62333e75eb82914ca6dd99ab0e3fd963293cb4a7
pablinho <pablinho.blanco@gmail.com>
2014-07-09 17:55:34 +0200

    externalizado arquivo configuración

--- conecta.php
+++ conecta.php
@@ -1,6 +1,7 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <?php
-$conexion = mysql_connect('localhost','montenoso5','cartosachador') or die("No se pudo realizar la conexion con el servidor.");
+
+require_once('conf.php');
+
+$conexion = mysql_connect($bbdd_host, $bbdd_user, $bbdd_password) or die("No se pudo realizar la conexion con el servidor.");
 mysql_query("SET NAMES 'utf8'", $conexion);
-mysql_select_db("cartografia" ,$conexion) or die("No se puede seleccionar BD"); 
-?>
+mysql_select_db($bbdd_name  ,$conexion) or die("No se puede seleccionar BD"); 
---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$bbdd_host = 'localhost';
+$bbdd_user = 'usuario';
+$bbdd_password = 'contrasinal';;
+$bbdd_name = 'montenoso_cartografia';
