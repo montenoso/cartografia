b7f98fc4741fc7b02dc223b974b67df2a403fe31
Montenoso <montenoso@gmail.com>
2014-07-09 17:27:54 +0200

    Update conecta.php
    Datos de conexión novos

--- conecta.php
+++ conecta.php
@@ -1,6 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <?php
-$conexion = mysql_connect('localhost','montenoso4','sachador') or die("No se pudo realizar la conexion con el servidor.");
+$conexion = mysql_connect('localhost','montenoso5','cartosachador') or die("No se pudo realizar la conexion con el servidor.");
 mysql_query("SET NAMES 'utf8'", $conexion);
-mysql_select_db("appmontenoso" ,$conexion) or die("No se puede seleccionar BD"); 
+mysql_select_db("cartografia" ,$conexion) or die("No se puede seleccionar BD"); 
 ?>
