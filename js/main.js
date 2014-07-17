46ddccc251b9b9331b45d74fbb6a94787f22a5b8
pablinho <pablinho.blanco@gmail.com>
2014-07-17 21:38:38 +0200

    restaurada versión simple cartodb

--- js/main.js
+++ js/main.js
@@ -36,7 +36,12 @@ $(document).ready(
     );*/
 
 
+ var url = 'http://montenoso2.cartodb.com/api/v2/viz/9ad57d16-0552-11e4-8ccb-0e10bcd91c2b/viz.json';
+ cartodb.createVis('map', url)
+ .done(function(vis, layers) {
+ }); 
 
+/*
     var map;
 
     var mapOptions = {
@@ -56,7 +61,7 @@ $(document).ready(
         );
       show_all_markers();
     });
-
+*/
 
   }
 );
