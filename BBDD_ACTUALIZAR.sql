c8354dccb077abf71f3cfa4f5da875d6736083fa
pablinho <pablinho.blanco@gmail.com>
2015-03-23 20:57:28 +0100

    eliminados antigos arquivos

--- BBDD_ACTUALIZAR.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE documento ADD pai INT;
-DROP TABLE tag;
-
-ALTER TABLE documento DROP COLUMN extension;
-ALTER TABLE documento DROP COLUMN peso;
-ALTER TABLE documento DROP COLUMN tema;
-ALTER TABLE documento ADD categoria VARCHAR(200 
