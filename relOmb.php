8d7f6b67cba209bd71a5b392261968718bed885b
Bimbiela <andreagaliza@hotmail.com>
2015-04-23 20:11:45 +0200

    Create relOmb.php

---
+++ relOmb.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+include("conecta.php");
+?>
+
+<div id="ficha">
+
+<br></br>
+Ombre: O monte veciñal nas fragas do Eume
+<br></br>
+
+
+<a href="http://montenoso.net/wp-content/uploads/2013/08/zona-MVMC-Ombre.jpg"><img alt="zona MVMC Ombre" src="http://montenoso.net/wp-content/uploads/2013/08/zona-MVMC-Ombre-300x225.jpg" width="300" height="225" /></a>
+</br>
+
+O 19 de agosto do 2013 xuntámonos parte do piñeiro de produción con dous comuneiros e irmáns de Ombre (Pontedeume). Eugenio (vicepresidente na actualidade) e Ramón, quen foi presidente durante anos da Comunidade de Veciños do Monte en Man Común de Ombre.
+
+Este monte veciñal en man común situado ao pé do río Eume destaca por ter montes inseridos nun parque natural, o das Fragas do Eume, o que vai ter certos condicionamentos relacionados coa existencia deste espazo natural. Para aqueles que aínda non coñezan esta zona, decirlles que as <a href="http://es.wikipedia.org/wiki/Fragas_del_Eume">Fragas do Eume</a> son un dos mellores exemplos de bosque atlántico en Europa, onde a presenza de carballos, castaños, ameneiros e bidueiros fan do lugar un dos mellores exemplos do que é unha fraga.
+
+Mais a presenza do eucalipto rompe con aquela visión romanticista herdada nos nosos imaxinarios. Mais xa voltaremos a cuestión dos eucaliptos, sempiterno tema na xestión dos montes galegos.
+
+Na actualidade, a CVMMC de Ombre, ten 184 comuneiros, que xestionan 84 hectáreas de monte. Tal e como recoñeceu Eugenio, “a participación non é moi alta” e cre que en parte está motivado porque o funcionamento das asembleas é “certamente caótico”. Aínda así, e tal como afirmou, a comunidade de veciños si ten presente ao monte como algo de seu. Durante a conversa, tanto Eugenio como Ramón queixáronse da falta de participación e lembraron como na última asemblea tiveron que suspendela ao non chegar ao quorum necesario.
+
+</br>
+
+<a href="http://montenoso.net/wp-content/uploads/2013/08/comuneiros.jpg"><img  alt="comuneiros" src="http://montenoso.net/wp-content/uploads/2013/08/comuneiros-300x225.jpg" width="300" height="225" /></a>
+</br>
+No referente á participación da xente moza, consideran que esta é positiva, tanto nas asembleas como na propia xunta rectora. Nesta aldea eumesa non houbo un despoboamento, e aínda se instalou xente moza, polo que o futuro da propia comunidade de montes está garantido; ten futuro por diante. Non ocorre o mesmo coa presenza de mulleres. Eugenio indicounos que a participación de mulleres como comuneiras representa en torno ao 5% do total, mais agardan que na vindeira asemblea poidan incorporarse mulleres á xunta rectora.
+
+
+
+Se nos preguntamos que é o que motivou a Eugenio a ser vicepresidente, el teno claro: “axudar ao presidente, que é un home moi maior. Se el o deixa, eu tamén. Eu creo no que fai e a miña función é botarlle unha man, mais estou en moitos temas e teño o tempo limitado, se o presidente o deixa eu tamén o fago”. E Ramón, quen fora presidente durante anos da Comunidade, apuntou que a súa motivación estaba “en poder axudar á parroquía e aos veciños, facer un uso axeitado do monte, que rendise no ben da comunidade”. Decíanos o ex-presidente que é importante que os veciños e veciñas traballen unidos, que ao final isto revirte no ben común da veciñanza.
+
+Ámbolos dous laiáronse durante a conversa da mextura, en certas ocasións, da presenza de intereses partidistas locais alleos á comunidade. Eugenio comentou que "non todo o mundo entende a democracia"; a isto Ramón engadiu que “a política é todo, e na xestión do monte, tamén a hai, mais o que non debería ser é que haxa rollos dos partidos políticos”. Quizais este feito tamén ten motivado a baixa participación da comunidade de veciños e veciñas.
+
+</br>
+
+<a href="http://montenoso.net/wp-content/uploads/2013/08/no-monte.jpg"><img alt="no monte" src="http://montenoso.net/wp-content/uploads/2013/08/no-monte-300x225.jpg" width="300" height="225" /></a>
+</br>
+Na conversa que mantivemos, tamén nos comentaron que algo que lles motivaba a continuar mantendo á comunidade de forma activa era que esta non pasase a depender da administración pública. Neste punto, cómpre lembrar que cando unha comunidade non é quen de ter unha xunta rectora e manter os mínimos legais que determina a Lei de Montes de Man Común de Galicia, a Xunta ten a potestade de xestionar este monte. Eugenio e Ramón coincidiron en que o monte debe seguir pertencendo comunidade, "o monte é noso, da comunidade de veciños" . En canto á relación coa administración, esta é fluida, mais queixábanse de que na maioría das ocasións a resposta da administración é “non hai cartos” e tamén lamentaban certa pasividade pola parte deles.
+
+
+Outro motivo que apuntaron da baixa partcipación está relacionado cun proceso xudicial dunha denuncia dun comuneiro durante a presidencia de Ramón. Este proceso xudicial foi complexo e levou sete anos, provocando o cansanzo da veciñanza, xa que xerou certa fractura social. En moitas ocasións non é tanto a decisión xudicial, senón o pouso que queda na comunidade. De feito Eugenio apuntaba “ que a xente non quere líos, quere que o monte reparta beneficios e se non o fai, que haxa investimantos na parroquia”.
+O uso do monte veciñal en Ombre cambiou. Antes da dictadura de Franco, o uso do monte era de pasto. Tiñan ovellas e aproveitaban o toxo. "Teñen morrido vacas que caían monte abaixo. Mesmo unha que era da casa", lembran os irmáns. Entón non había un aproveitamento madereiro industrial.
+
+</br>
+
+<a href="http://montenoso.net/wp-content/uploads/2013/08/mapa-ombre.jpg"><img  alt="mapa-ombre" src="http://montenoso.net/wp-content/uploads/2013/08/mapa-ombre-194x300.jpg" width="194" height="300" /></a>
+</br>
+Durante o franquismo a política reforestadora foi "expoliando" os montes veciñais, que deixaron de ser de seu, da veciñanza, pasando a ser deses outros que foron incapaces de ver esa simbiose que existía entre os comuneiros e o seu monte, decidindo así de forma unilateral o que era beneficioso para a comunidade sen telos en conta. Este tipo de política pon de manifesto unha das grandes presións ás que as Comunidades de Montes tiveron que facer fronte.
+
+
+
+Na actualidade o principal uso do monte é o sector madereiro. A plantación de eucaliptos. Tanto Eugenio como Ramón saben da problemática que trae o eucalipto: o esgotamento do chan, que son moi invasivos, que absorven auga de máis, as plagas, etc. Mais este tipo de aproveitamento do monte é o único xeito que teñen de sacar recursos económicos para investir na parroquia. Os terreos nos que están os seus montes son moi en pendiente, polo que ven difícil adicalo a outra cousa para sacar o rendemento económico necesario.
+
+O Parque Natural obriga ao monte a non replantar eucaliptos, polo que os comuneiros plantexan receber a cambio unha especie de aluguer ou ver de que xeito poden facer un sistema que lles repercuta economicamente para a sustenibiidade da MVMC. Quéixanse que tanto o Parque Natural coma a administración, poñen normas e restrinxen usos, mais non ofrecen outras posibilidades nin dan outras opcións viables para o mantemento do MVMC.
+
+Tal e como indicamos neste relato, parte do monte está dentro do Parque Natural, co quen teñen unha relación cordial e dialogante. "Que mellor cousa que o monte sexa un lugar no que poida disfrutar a xente", din. Máis aparece de novo a presenza dos eucaliptos; algúns deles están enfermos, co gorgoxo, e secando. Por iso querían talalos, mais a Xunta e o Parque non lles deixaban nun principio. Agora están en negociacións. "Non é cuestión de sacar diñeiro, o tema é que están secos e o Parque, para estar ben, non pode ter unha cousa así de fea e abandoada" decíanos Eugenio.
+
+</br>
+</br>
+<a href="http://montenoso.net/wp-content/uploads/2013/08/falando-no-monte.jpg"><img  alt="falando no monte" src="http://montenoso.net/wp-content/uploads/2013/08/falando-no-monte-300x225.jpg" width="300" height="225" /></a>
+
+A procura de solucións semella unha práctica desta comunidade, entre algunha das ideas atópase a de aproveitar uns muíños que teñen dentro do monte, polo que lle propuxeron ao Parque que rehabiltasen estas construccións tecnolóxicas, o que pemitiría entender os usos que tiña o monte e dinamizar unha ruta e paseo. Propuxéronlle tamén ao Parque facer uns depósitos de auga de cara a posibles incendios, mais non receberon resposta polo de agora.
+
+
+Dentro dos montes veciñais hai unha canteira de pedra. Estaba en réxime de aluguer, mais agora, debido a situación de crise económica, este aluguer é pagado cun intercambio de servizos. Cando hai que limpar as pistas, é a xente da canteira coas súas máquinas quen o fai. Son unha comunidade aberta a solucións e novas posiblilidades.
+Unha cuestión sempre importante no referente á teoría dos comúns é que os comúns deben server para unha redistribución da riqueza; se isto non se produce, ao mellor só nos estamos situando ante unha práctica de innovación social. No caso específico que nos ocupa, o do monte de Ombre, a redistribución desta riqueza adoitou facerse mediante obras na parroquia. A meirande parte das pistas que existen na parroquía foron feitas co diñeiro da CMVMC. Á vez, tamén construiron o local social da asociación de veciños, o que permitiu afondar na idea de crear espazos comunais nos que facer xuntazas, festas e encontros entre a veciñanza. A relación entre a comunidade de montes e a asociación de veciños é moi estreita, mais non son o mesmo. Ás veces hai certa confusión en diferenciar as dúas estructuras. Aínda que os ámbitos de actuación sexan en ámbalas dúas estructuras a parroquia e os veciños, non son o mesmo.
+
+</br>
+</br>
+<a href="http://montenoso.net/wp-content/uploads/2013/08/linde.jpg"><img  alt="linde" src="http://montenoso.net/wp-content/uploads/2013/08/linde-300x225.jpg" width="300" height="225" /></a>
+
+Eugenio e Ramón son partidarios de non repartir o diñeiro obtido dos montes entre os comuneiros. Hai uns meses fixeron un reparto de diñeiro dunha tala, e tocoulles a 150 € por comuneiro; así o considerou a asamblea. "Esto non soluciona nada dunha economía familar" afirmaron, mais a democracia funciona así. Ámbolos dous son partidarios de reinvertir o diñeiro na parroquia e no melloramento dos montes. Tamén destaca o uso social dos seus recursos económicos. O diñeiro que sae do monte tamén ten axudado ás persoas máis desfavorecidas, tanto da súa parroquia coma de outras. Así, teñen colaborado con persoas ás que lles ardeu a casa, ou a quen lles morreron os animais que sustentaban a economía familiar. Estes son dous exemplos.
+
+Os retos desta comunidade son amplos, mais existe vontade por traballar pola comunidade e no mantemento do monte en man común. Quedan moitos relatos por escribir do devir desta comunidade, mais so a propia veciñanaza, a propietaria deste ben, será quen de ir trazándoos. Nós, pola nosa parte, abrimos unha vía de colaboración e agardamos seguir traballando con eles.
+
+</div>
