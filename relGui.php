4e3c6a86785e027d9a5dd1384986fb52894c0cb7
Bimbiela <andreagaliza@hotmail.com>
2015-04-23 20:07:08 +0200

    Create relGui.php

---
+++ relGui.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include("conecta.php");
+?>
+
+<div id="ficha">
+    
+    <p>Guillade. Dende as cinsas...</p>
+
+    <p>O Camilo (Vogal da comunidade de montes) sentounos a tomar café. Eu estaba co Alfonso e co Diego, compañeiros e amigos da Asociación Cultural Abarlovento. Foinos poñendo ao día sobre a realidade cotiá da parroquia, a situación do concello, Ponteareas... coa idea de facernos a necesaria a <i>composición</i> do lugar. Non había grandes sorpresas no discurso, pero si a constatación das dificultades de xestión que supón obrar nestas realidades de poucxs veciñxs con intereses e problemas que veñen de antigo e moi marcados pola política local.</p>
+
+    <p><a href="http://montenoso.net/wp-content/uploads/2013/09/guillade1.jpg"><img class="aligncenter size-large wp-image-1385" alt="guillade1" src="http://montenoso.net/wp-content/uploads/2013/09/guillade1-1024x768.jpg" width="620" height="465" /></a></p>
+
+    <p>A Comunidade de Montes Veciñais en Man Común de Guillade non ten problemas coa de Mouriscados (de feito, están a colaborar nun itinerario arqueolóxico onde o petrogligo mais salientables está en terreos de Guillade). Co resto de parroquias limítrofes: Uma en Salvaterra; e Celeiro, Cumiar e Oliveira en Ponteareas, hai algúns roces: problemas de límites. Aínda que non van a máis.</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/P1050007.jpg"><img class="aligncenter size-large wp-image-1332" alt="P1050007" src="http://montenoso.net/wp-content/uploads/2013/09/P1050007-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>Dentro de Guillade a principal dificultade é o Heliporto. Si, si: Un aeroporto privado para helicopteros que está en terreo comunal. Fora vendido a un particular pouco antes da aprobación da Lei de Montes Veciñais da Xunta. O terreo recuperárono mais están en litixios por una nave do heliporto que construiron nos últimos anos.Sumados aos problemas cos límites temos un feixe de contenciosos que non levan a nada mais que fan gastar moitos recursos e enerxías.</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/P1050079.jpg"><img class="aligncenter size-large wp-image-1331" alt="P1050079" src="http://montenoso.net/wp-content/uploads/2013/09/P1050079-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>En Guillade a idea é seguir co iniciado anos atrás: Daquela conseguírase que os comuneiros aceptaran plantar piñeiros e algunhas frondosas: carballos, castaños e mesmo algunha cerdeira, evitando a plantación de alcolitos. Tralos lumes do 2006, que arrasaron con todo, retoñaron os piñeiros que había. Hoxe o obxectivo é intentar consolidar esa política, aínda que esta xa sexa unha labor titánica e nos últimos meses non haxa quen se ocupe de desbrozalo monte. De feito Hoxe non se atopan facilmente os manantiais de auga.</p>
+
+    <p>Aínda que mora en Compostela, o Camilo ven tódalas fins de semanas. Leva pouco máis dun ano na nova xunta directiva do monte, que se ocupa de xestionar o día a día da comunidade. Para os temas máis complexos, faise unha asemblea. Fainos de guía pola parroquia e lévanos visitar algunhas persoas con relación co monte veciñal.</p>
+
+    <p>A comunidade ten páxina <a href="http://montesdeguillade.blogaliza.org/">http://montesdeguillade.blogaliza.org/</a> e agradécese ver que, aínda que sinxela, ten todo o que unha web dunha Comunidade de Montes precisa: as actas das asembleas accesibles, cartografías do monte, unha reportaxe dos seus paseos <i>(derivas!)</i> polos montes, artigos sobre o patrimonio, un apartado para ideas para o monte, e máis...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/P1050108.jpg"><img class="aligncenter size-large wp-image-1333" alt="P1050108" src="http://montenoso.net/wp-content/uploads/2013/09/P1050108-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>Falamos co Antone. Home culto, fillo de emigrantes en Lisboa, voltou a Guillade aos 18 anos. É agora o secretario da comunidade. Un grande defensor do valor do comunal.  Fálanos dun xeito trascendente, filosófico, sobre a importancia que ten para a parroquia e para a "tribo" estes terreos. Nós facémonos eco da sinceridade que desprenden as súas rotundas palabras. Alerta de que "a comunidade" das persoas e dos traballos entorno do monte están a desaparecer: a maioría dos servizos os están a realizar unhas poucas empresas especializadas.</p>
+    <p>Unha botella de viño despois (grazas, Antone) saímos cara a casa do Jota (dise "iota"). O Jota, rapaz novo. É o único da parroquia que vive do campo ao 100%. Ten vacas rubias, estabuladas. Fai algúns meses solicitou á asemblea da comunidade de montes a cesión dunha parte do terreo para ter unha explotación de vaca cachena, vianesa e caldelá, todas elas vacas de monte. Ían servir  para o desbroce do monte. Son vacas que só se alimentan da vexetación, sen precisar de piensos. Contribúen ao mantemento do monte limpo e sen riscos de lumes.</p>
+    <p>A asemblea non obstante negoulle fai varios meses a explotación en monte comunal por unha escasa marxe. O feito é que moita da xente, ao solicitalo o Jota tamén solicitaron cesións de terreos: para poñer cebola, pataca... En realidade non o ían cultivar porque xa non o fan de seu, mais pedironno. Por molestar. Disque moitxs se opuxeron por envexa, para evitar que o Jota se "enriquecera" co "seu" monte. Non entenderon que o beneficio era mutuo: para el e para o monte que é de todxs. Agora o Jota ten que agardar a aprobación para final de ano do Plan de Usos do Monte que está a realizar a nova xunta. E a ver se daquela...</p>
+
+    <p> <a href="http://montenoso.net/wp-content/uploads/2013/09/P1050120.jpg"><img class="aligncenter size-large wp-image-1337" alt="P1050120" src="http://montenoso.net/wp-content/uploads/2013/09/P1050120-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>agradecidos polo tempo compartido e imos cara xunto da Verónica. Ela foi vogal do Monte Veciñal e mesmo candidouse nas últimas eleccións á presidencia, perdendo só por un voto. Mírase que é unha persoa ben botada para adiante (e co necesario punto de atrevemento e ousadía) xa que foi  todo un reto o de presentarse alí. Muller e nova. De feito, tras estar un ano de vogal preferiu deixarlle a titularidade ao seu marido xa que discutía moito nas asembleas e estaba a afectarlle nas súas relacións veciñais. Con "vellos máis rancios", que disque abondan na asemblea, o levaba ben. Sabía que eran así e punto. Cousa da idade. Pero con mozxs da súa quinta que a menosprezaban non podía. Así que tras varias discusións encendidas decidiu que era mellor ceder a praza na casa.</p>
+
+    <p>Con todo, o seu exemplo serviu, e as viúvas de moitos comuneiros, que ían ás asembleas pero non votaban, comezaron a cambiar a titularidade para poder asisitir e votar. Benvidas sexan. E agora hai outra muller nova... Hai actitudes que só van ir mellorando segundo pasen os anos e desapareza esa xeración que non é quen de adaptarse aos cambios...reflexiona ela.</p>
+
+    <p>Por último visitamos a Xosé. (Camilo despidese, que ten que ir bañar á cativa e nós quedamos co tesoureiro da comunidade). Xosé ten unha visión menos positiva e non lle ve moito futuro á isto da xestión veciñal dos montes. Pouca poboación, pouca comunidade e pouco interese na maioría da xente que xa non o ve coma un recurso. Difícil papeleta...</p>
+
+    <p><a href="http://montenoso.net/wp-content/uploads/2013/09/P1050123.jpg"><img class="aligncenter size-large wp-image-1334" alt="P1050123" src="http://montenoso.net/wp-content/uploads/2013/09/P1050123-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>Marchamos de Guillade cunha sensación estrana, por ter coñecido xente moi interesante e con ganas de facer. Pero sabemos de certo que amosáronnos a mellor cara da Comunidade de Montes. Faltounos falar cos mais repunantes...coidamos...</p>
+
+</div>
