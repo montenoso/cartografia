99d07180ad7a90bfd663fcf34208d5f4c90715d3
Bimbiela <andreagaliza@hotmail.com>
2015-04-23 20:02:48 +0200

    r

---
+++ relArg.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+include("conecta.php");
+?>
+
+<div id="ficha">
+       <form id="form1" name="form1" method="post" action="http://montenoso.net/cartografia/">
+                    <input type="image" src="http://montenoso.net/cartografia/images/pinhaVoltar.png""  a href="#" onclick="javascript:document.form1.submit();" style="cursor:pointer;"> 
+                </form>
+
+    <p>Comúns situados en Argozón</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSCN2067.jpg"><img  alt="DSCN2067" src="http://montenoso.net/wp-content/uploads/2013/09/DSCN2067-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>Saímos de Chantada cedo. Pola mañán, o frescor. O verde, e máis verde en agosto, en plena vaga de lumes. Falamos de historias familiares da zona, dun certo apego ao Monte Faro. Estiveramos noutras dúas ocasións co Jado ou @ventonoso, o Guerra e mailo Jorge ou @HartazgoRedux, da CMM e a asociación de Veciñxs respectivamente. Daquela apresentarámoslles O monte é noso, recolleramos os datos para o cuestionario que hoxe habemos pasar, mais preparáramos a xornada de hoxe. Soan Fanny+Alexander no coche e han estar presentes todo o día. Imos cara arriba, polas curvas do Val, poñéndonos ao día sobre a CM...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/TagsArgozón1.jpg"><img  alt="TagsArgozón" src="http://montenoso.net/wp-content/uploads/2013/09/TagsArgozón1.jpg" width="634" height="638" /></a></p>
+    <br></br>
+
+    <p>... E disque xa coñecían o proxecto a través do twitter, que a Xunta Reitora da comunidade está hiperconectada. Nós revisaramos tamén as súas noticias na páxina da ORGAMVMC. Pareceunos raro que non estiveran adscritxs, nen mancomunadxs. Imos cara unha illa na Serra do Faro, entre as comarcas de Chantada e O Deza, na conca fluvial do Río Miño, conectada á rede. Facemos memoria e si, tiveramos algún debate con @ventonoso. Diferente pé. Empatía...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSCN1980.jpg"><img  alt="DSCN1980" src="http://montenoso.net/wp-content/uploads/2013/09/DSCN1980-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>...Chegamos á Casa Reitoral, marabillosa como espazo de encontro, como CSO, como sede de cursos e charlas para a parroquia, como espazo ótimo pra ver o fútbol, pra aniversarios, pra comidas populares. Conserva a estrutura da vella casa do cura das abellas e dúas teles, boa conexión á rede, mesmo bar. Varios espazos. Nesta casa seica a habitación da traballadora do fogar era maior ca do cura, haiche moito ye-yé, que diría o outro. Sorprende a variedade de idades das persoas que van chegando. Unhas 25. De 14 a 85 anos, cremos. Preguntamos pola nosa xeración, onde andarán. E din que teñen resaca das festas de Chantada, que este ano levaron un carro no Folión: <i>O val vai traballar</i>. Raramente se implican nas actividades que se organizan na parroquia. Este ha ser un tema recurrente ao longo do día. Como contarllelo proxecto. Quen vimos sendo e o que facemos e o por que nos interesa a experiencia do que chaman Monte Común...</p>
+
+    <iframe src="http://www.slideshare.net/slideshow/embed_code/26243775" height="400" width="476" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+
+    <p>... é posible aplicar este conxunto de saberes a outros eidos? No seu caso adícanse ó sector madeireiro. Máis teñen ideas de futuro. Como s xestionan outros bens da parroquia? Como se dan e artellan as redes de afectos? Por qué hai premios nóbeles que falan sobre a xestión dos bens comúns? e que din? Que lles parece o proxecto e qué lles interesa del? Chegan o Humberto maila Astrid da Tele20. Viron da que subían dúas columnas de fume arriba das casas e veñen preocupadxs. Xa chamaran informando. Falan co presidente, animamos ás veciñxs a que lles conten impresións e non queren. Arriba non pillan a Tele20. Falamos con elxs un anaco...</p>
+
+    <object width="420" height="315" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"><param name="allowFullScreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="src" value="//www.youtube.com/v/Tp97w3Gg-ms?version=3&amp;hl=es_ES&amp;rel=0" /><param name="allowfullscreen" value="true" /><embed width="420" height="315" type="application/x-shockwave-flash" src="//www.youtube.com/v/Tp97w3Gg-ms?version=3&amp;hl=es_ES&amp;rel=0" allowFullScreen="true" allowscriptaccess="always" allowfullscreen="true" /></object>
+
+    <p>...e quédannos cousas por lles contar. Sobre todo no tocante ás residencias, que non son tal senón Intervencións no monte comunal ou na parroquia en base a problemáticas ou necesidades específicas que teñan. E é o aspecto que máis lles interesa hoxe xs comuneirxs. Falamos de <a href="http://opensourceecology.org/">http://opensourceecology.org/</a> e das posibilidade  de artellar un taller de mecánica opensource pra construir ou amañar maquinaria de maneira autoxestionada. Doutravolta saen na conversa xs mozxs da parroquia. Podería ser un xeito de reclamo para o persoal, bastante mañoso. E os custes de obtención destas ferramentas haberíanse reducir moito. Se hai moitxs mozxs que fedellan nos seus coches, non poderían facelo tamén cos tractores?</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/IMG_3734.jpg"><img  alt="IMG_3734" src="http://montenoso.net/wp-content/uploads/2013/09/IMG_3734-1024x764.jpg" width="620" height="462" /></a>
+
+    <p>...son xente receptiva. O presidente fala bastante, é un home listo, un <i>perro-viejo, sabe do que fala</i><i>. Conflitos coa administración, con empresas eólicas. </i> Propoñemos lanzar algunhas preguntas de cara ao cuestionario. O que queremos con isto e respostar a unha serie de cuestións relacionadas cos tres pés dos comúns: as persoas/comunidade, o recurso/monte, a gobernanza. Máis tamén coa transferencia destes saberes e a comunicación dos mesmos.</p>
+    
+    <blockquote>
+    <p style="text-align: right;"><a href="montenoso.net/formularioMontes">[Se és comuneirx, ti tamén podes cubrir o formulario]</a></p>
+    </blockquote>
+    
+    <p>...mais pasa o tempo e comeza a falarse que hai un longo camiño polo sendeiro de Freán até o pinar onde será o xantar popular: Onde se calzan as medias, disque. A xente mobilízase, case é a unha. Hai quen sube en coche, que non nos pareza mal, que non lle dan as pernas, que moitas veces subiron cas vacas, ou á romaría do Faro, cargadxs coma burras. Vende-lo xamón e os ovos, roza-los toxos, ó sana barigas...Todo ben, ides no coche e habemonos ver arriba. Agardade pra comer.</p>
+
+    <p>...[escoita <a href="http://montenoso.net/wp-content/uploads/2013/09/OGuerra.m4a">O Guerra</a>]
+
+    <p>Mentres subimos, unha non pode deixar de mirar a espectacular natureza que nos rodea, o val ao fondo e os piñeiros ao noso carón. Preguntamos se sería posible plantar outras árbores ou adicar o monte a outras actividades e dinnos que é difícil, que é unha zona alta, en torno aos 900 metros de altitude e que poucas especies se poden dar nesa altitude. Cóntannos que estas 378 hectáreas de monte en man común pertenceron a unha condesa, quen foi vendendo as parcelas máis productivas e que as que non foi capaz de vender, deixounas en monte común. Repítese outra vez esa idea do monte comunal como "terra inculta" e os labradíos como "terras cultas". O sol apreta. Toxos, fentos, carpazas, queirogas e xestas. Os coellos, lebres, lobos, raposos e toupas alén de cotovías, lavercas, paspallás, pombas e ferreiros. Subimos. Carballos, cerquiños, abidueiros e bruñeiros. Arandeiras, cabaciñas ou xabroneiras. Arriba. Os piñeiros. Uns quince kilómetros. De historias...</p>
+
+    <p>... [escoita <a href="http://montenoso.net/wp-content/uploads/2013/09/AAlba.m4a">A Alba</a>]</p>
+
+    <p>O sol. Suor. Querías monte, aí o tes. Súbeo. A auga das fontes, ocultas pola maleza. Costa. Calor. Suor. Chegamos ao lugar onde calzaban as medias as rapazas que ían ou viñan da Romaría do Faro. A primeira imaxe que temos de chegar ao piñeiro...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSCN2121.jpg"><img  alt="DSCN2121" src="http://montenoso.net/wp-content/uploads/2013/09/DSCN2121-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>...costou a subida, costou. Aquí estamos o Luís e máis nós. Unhas trinta persoas, moitas caras novas, están dispostas a comer, xa con viño, agardando. Logo sucédense un sinfín de empanadas, conservas, embutidos, queixos, ensaladas e postres varios. Licores. E cando nos dispoñemos a pegar unha soneca á sombra propoñen baixar de seguido, que hai que facer varias paradas antes de voltar á Casa Reitoral e hai que munguir. Emprendemos logo unha nova marcha. De historias...</p>
+
+    <p>...[escoita <a href="http://montenoso.net/wp-content/uploads/2013/09/APili.m4a">A Pili</a>]</p>
+
+    <p>...[escoita O Luís]</p>
+
+
+    <p>E seguimos, o camiño, desta vola baixa, agradécese :). Os eólicos están ao carón. A comunidade moito leva loitado nos xuzgados por obter unha compensación xusta pola presenza destes xigantes. "E non é sinxelo" dinos o presidente, quen nos vai relatando ese particular vía crucis coa xustiza e coa administración, que ás veces semella que está máis ao servizo duns lobbies que do pobo. Mais cando a sentenza sae favorable á comunidade e a empresa eólica ten que pagar a compensación, xurde o gran debate. Que facer con eses cartos? dividilos e repartilos coa comunidade ou investilos en proxectos que redunden na comunidade. Pois ben, escolleron a última opción. Ben, ben, ben feito. Andan a ver se é posible facer algo de biomasa con pellets, neso están.  Mais hai que ter sentidiño, e semella que o teñen.</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSCN2071.jpg"><img alt="DSCN2071" src="http://montenoso.net/wp-content/uploads/2013/09/DSCN2071-1024x768.jpg" width="620" height="465" /></a>
+
+
+    <p>...A fonte dos Meniños, onde viñan traer nenos e nenas de moitas parroquias e mesmo de fóra, ante as propiedades da fonte milenaria onde as crianzas entraban, deixaban as roupas e algunha moeda enriba dunha pedra e pasaban por baixo dela varias veces. A Capela de Santa Eufemia, desaparecida, que fora traída pedra a pedra, tella a tella; Os muíños, parados nesta época, ou como exportar máis e máis enerxía custe o que custe; Os regatos que nos ían descubrindo, ocultos entre a maleza. O monte, baixamos, o val, baixamos...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSC00033.jpg">
+    </a> <a href="http://montenoso.net/wp-content/uploads/2013/09/DSC00103.jpg"><img  alt="DSC00103" src="http://montenoso.net/wp-content/uploads/2013/09/DSC00103-1024x576.jpg" width="620" height="348" /></a>
+
+    <p>...Hai moito traballo por facer na Casa Parroquial, poderían implicarse aí tamén xs novxs. Que disque poñen puntas en Chantada para o folión de carros cando non as puxeron nunca na casa parroquial. Moléstannos un pouco estes comentarios e facémolo saber.  Un whatsapp de Iván. "Déixote o k leron no palco cando pasamos ca carroza". Lemos cada unha un anaco. "O val vai traballar". Rimos...</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/IMG_3802.jpg"><img  alt="IMG_3802" src="http://montenoso.net/wp-content/uploads/2013/09/IMG_3802.jpg" width="800" height="600" /></a>
+
+    <p>...Vemos as imaxes do roteiro, comentámolo. Soan Fanny+Alexander de novo. Dicimos que namentres se ven as imaxes nós poñemos cancións a peso. Pídennos Ana Carolina. "Gosto de homens e de mulheres", Bossa Nova, suben o volume. Os licores. Temos distintos sindicatos labregos de referencia e coxeamos de diferente pé, din. Propoñemos imprentar algunhas das imaxes de hoxe pra poñelas na Casa. Acendemos. Sae a idea dunha exposición paralela á do Museo. Van vir a Vigo. Agora teñen que ir munguir. Marcho que teño que marchar. Outras a facer as tarefas do cole que en verán non tocaron un libro. Nós para Chantada, a morrer no sofá. Que estamos convidadxs ó magosto e á festa de outubro. Que as dúas portas están abertas. Pedimos ás últimxs persoas que quedan que da que marchan senten un intre e nos digan así sen pensar o que se lles ven á mente cando pensan en Monte Común.</p>
+
+    <a href="http://montenoso.net/wp-content/uploads/2013/09/DSCN1996.jpg"><img  alt="DSCN1996" src="http://montenoso.net/wp-content/uploads/2013/09/DSCN1996-1024x768.jpg" width="620" height="465" /></a>
+
+    <p>Como toma de contacto estivo moi ben, máis deixamos moita sementa por abrollar, queda moito rego aberto de cara a...</p>
+
+    <p>...Entender as redes da parroquia. A representación de comuneirxs foi reducida. Falamos bastante de como se organiza a Xunta Reitora, pero precisamos visualizar os fíos invisibles entre esta e as comuneirxs.</p>
+
+    <p>...Afondar máis na organización e nos aspectos de autoxestión da mesma.</p>
+
+    <p>...Contactar directo ca xente nova da Parroquia. Saber que lles interesa e como ven a continuidade da propiedade compartida do monte.</p>
+
+    <p>...Observar as relacións exteriores da comunidade, nomeadamente con outras comunidades en Chantada e con organizacións de CMM.</p>
+
+    <p>Imos seguir.</p>
+
+</div>
